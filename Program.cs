0772e1fc6b3b9cc993b64338adaf9555b49c8311
NastyaUn <tarasova121200@gmail.com>
2022-01-08 12:07:42 +0400

    Singleton

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Паттерн_3_Singletone
+{
+    public class Autor
+    {
+        static Autor autor;
+        protected Autor()
+        {
+            Console.WriteLine("Welcome, autor!");
+        }
+        public static Autor Instance()
+        {
+            if (autor == null)
+            {
+                autor = new Autor();
+            }
+            else 
+            { Console.WriteLine("Autor already exists"); }
+            return autor;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Autor p = Autor.Instance();
+            Autor p1 = Autor.Instance();
+        }
+    }
+}
